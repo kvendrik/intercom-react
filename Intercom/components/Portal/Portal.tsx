690035ac3b3eed7ae7fa2274629602b06399f50a
Koen Vendrik <k.vendrik@gmail.com>
2018-06-24 12:34:13 +0200

    fixes component type

--- Intercom/components/Portal/Portal.tsx
+++ Intercom/components/Portal/Portal.tsx
@@ -5,7 +5,7 @@ export interface Props {
   children: React.ReactNode;
 }
 
-export default class Portal extends React.Component<Props, never> {
+export default class Portal extends React.PureComponent<Props, never> {
   render() {
     const {children} = this.props;
     return ReactDOM.createPortal(children, document.body);
