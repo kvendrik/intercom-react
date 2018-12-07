3788f42fadc01da65506a81a15bd6d866fbc55a4
Koen Vendrik <k.vendrik@gmail.com>
2018-12-07 12:44:38 -0500

    targets intercom app to listen for mutations instead of whole frame body

--- src/components/BorderlessFrameListener/BorderlessFrameListener.ts
+++ src/components/BorderlessFrameListener/BorderlessFrameListener.ts
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {bind} from 'lodash-decorators';
 
 export interface BorderlessFrameSizes {
   width: string;
@@ -15,17 +16,22 @@ const LAUNCHER_SIZE_PIXELS = 60;
 const LAUNCHER_MARGIN_PIXELS = 20;
 
 class BorderlessFrameListener extends React.Component<Props, never> {
-  private observer: MutationObserver = this.getObserver(
-    this.props.frame,
-    this.props.onSizesUpdate,
-  );
+  private observer: MutationObserver | null = null;
 
   componentWillMount() {
+    const {frame} = this.props;
+    this.observeNode(
+      frame.contentWindow!.document.body,
+      this.handleBodyMutation,
+      {
+        childList: true,
+      },
+    );
     this.injectCustomStyles();
   }
 
   componentWillUnmount() {
-    this.observer.disconnect();
+    this.cleanObserver();
   }
 
   render() {
@@ -49,56 +55,80 @@ class BorderlessFrameListener extends React.Component<Props, never> {
     contentWindow!.document.head!.appendChild(node);
   }
 
-  private getObserver(
-    frame: HTMLIFrameElement,
-    onSizesUpdate: Props['onSizesUpdate'],
+  private observeNode(
+    nodeToObserve: HTMLElement,
+    onMutation: (mutations: MutationRecord[]) => void,
+    options: MutationObserverInit,
   ) {
-    const {launcher} = this.props;
-    const frameBody = frame.contentWindow!.document.body;
+    this.cleanObserver();
 
-    const observer = new MutationObserver(mutations => {
-      for (const {target} of mutations) {
-        const node = target as HTMLIFrameElement;
+    const observer = new MutationObserver(onMutation);
+    observer.observe(nodeToObserve, options);
 
-        if (
-          !node.classList.contains('intercom-borderless-frame') &&
-          !node.classList.contains('intercom-notifications-frame')
-        ) {
-          continue;
-        }
-
-        const {
-          style: {height},
-          offsetWidth,
-        } = node;
-
-        const finalWidth = launcher
-          ? offsetWidth + LAUNCHER_MARGIN_PIXELS
-          : offsetWidth;
-        let finalHeight = height || '0px';
-
-        if (height && launcher) {
-          finalHeight = addMarginToPixels(
-            height,
-            LAUNCHER_SIZE_PIXELS + LAUNCHER_MARGIN_PIXELS * 2,
-          );
-        }
-
-        onSizesUpdate({
-          width: `${finalWidth}px`,
-          height: finalHeight,
-        });
-
-        return;
-      }
-    });
-
-    observer.observe(frameBody, {
-      attributes: true,
-      subtree: true,
-    });
-
+    this.observer = observer;
     return observer;
+  }
+
+  private cleanObserver() {
+    const {observer} = this;
+    if (observer) {
+      observer.disconnect();
+    }
+  }
+
+  @bind()
+  private handleBodyMutation([{target: body}]: MutationRecord[]) {
+    const intercomAppNode = (body as HTMLElement).querySelector(
+      '.intercom-app',
+    );
+    this.observeNode(
+      intercomAppNode as HTMLElement,
+      this.handleIntercomAppMutation,
+      {
+        attributes: true,
+        subtree: true,
+      },
+    );
+  }
+
+  @bind()
+  private handleIntercomAppMutation(mutations: MutationRecord[]) {
+    const {launcher, onSizesUpdate, frame} = this.props;
+
+    for (const {target} of mutations) {
+      const node = target as HTMLIFrameElement;
+
+      if (
+        !node.classList.contains('intercom-borderless-frame') &&
+        !node.classList.contains('intercom-notifications-frame')
+      ) {
+        continue;
+      }
+
+      const {
+        style: {height},
+        offsetWidth,
+      } = node;
+
+      const finalWidth = launcher
+        ? offsetWidth + LAUNCHER_MARGIN_PIXELS
+        : offsetWidth;
+      let finalHeight = height || '0px';
+
+      if (height && launcher) {
+        finalHeight = addMarginToPixels(
+          height,
+          LAUNCHER_SIZE_PIXELS + LAUNCHER_MARGIN_PIXELS * 2,
+        );
+      }
+
+      onSizesUpdate({
+        width: `${finalWidth}px`,
+        height: finalHeight,
+      });
+
+      return;
+    }
   }
 }
 
